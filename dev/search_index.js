4879349870a68341e58c5f612bf249c59fa6ae46
Documenter.jl <documenter@juliadocs.github.io>
2023-03-01 10:37:20 +0000

    build based on 22c019a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AlphaConformers","category":"page"},{"location":"#AlphaConformers","page":"Home","title":"AlphaConformers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AlphaConformers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AlphaConformers]","category":"page"}]
+}
