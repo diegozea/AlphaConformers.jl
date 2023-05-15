b7fc9d244b7678955da8c14f72fbf61bd236c4d3
Diego Javier Zea <diegozea@gmail.com>
2023-05-15 16:08:10 +0200

    TMalign and clusters

--- scripts/first_draft.jl
+++ scripts/first_draft.jl
@@ -120,7 +120,7 @@ function create_pdb_folder(search_results::DataFrames.DataFrame)
         target = row[:target]
         # Split target into pdb_code and chain_code
         fields = split(target, '_')
-        pdb_code = fields[1]
+        pdb_code = String(fields[1])
         if length(fields) == 2
             chain_code = String(fields[2])
         else
@@ -128,7 +128,7 @@ function create_pdb_folder(search_results::DataFrames.DataFrame)
         end
         # Define original and local file paths
         original_pdb_path = joinpath(pdb_folder, pdb_code)
-        local_pdb_path = joinpath(local_pdb_folder, pdb_code)
+        local_pdb_path = joinpath(local_pdb_folder, target)
         # Read only specific chain and write to local file
         # occupancyfilter is needed to avoid the duplicated residue warnings with TMalign
         chain = MIToS.PDB.read(original_pdb_path, MIToS.PDB.PDBFile, 
@@ -145,6 +145,8 @@ local_pdb_folder = create_pdb_folder(search_results)
 
 # ---
 
+#=
+
 # cluster structures using foldseek and see if the conformations are located in different clusters
 
 function run_foldseek_clustering(local_pdb_folder::String)
@@ -157,6 +159,8 @@ function run_foldseek_clustering(local_pdb_folder::String)
 end
 
 run_foldseek_clustering(local_pdb_folder)
+
+=#
 
 # I made just a few attempts with one example, but it seems that foldseek cannot 
 # effectively distinguish between conformations belonging to different clusters.
@@ -172,6 +176,8 @@ open("chain_list", "w") do io
     end
 end
 
+#=
+
 # run TMalign with the -dir option:
 
 dir_path = abspath(local_pdb_folder)
@@ -184,6 +190,8 @@ chain_list_path = abspath("chain_list")
 run(pipeline(`/store/EQUIPES/AMIG/MEMBERS/diego.zea/bin/TMalign -dir $dir_path $chain_list_path`, 
     stdout="first_targets.out", stderr="first_targets.err"))
 tmalign_out = read("first_targets.out", String)
+
+=#
 
 """
     parse_tm_align_output(tm_align_output::String)
@@ -220,6 +228,8 @@ function parse_tm_align_output(tm_align_output::String)
     return parsed_alignments
 end
 
+#= 
+
 tmalign_data = DataFrames.DataFrame(parse_tm_align_output(tmalign_out))
 
 rmsd_mat = PairwiseListMatrices.from_table(tmalign_data[:,1:3], false)
@@ -233,21 +243,203 @@ rmsd_chains = names(rmsd_mat)[1]
 rmsd_chains[rmsd_clusters .== 1]
 rmsd_chains[rmsd_clusters .== 2]
 
+=#
+
 # ---
+
+# Use the UniProt mapping to filter out known conformations from the query protein
+# in order to properly evaluate the pipeline and to look for known conformations
+# of the proteins showing similar structures to the query protein.
+
+download("ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/flatfiles/csv/pdb_chain_uniprot.csv.gz", 
+    "pdb_chain_uniprot.csv.gz")
+sifts_up_data = CSV.File("pdb_chain_uniprot.csv.gz", comment="#") |> DataFrames.DataFrame
+
+# create a function to look up the UniProt accession for a given PDB code and chain (if available)
+function get_uniprot_acc(pdb_file::String, data::DataFrames.DataFrame=sifts_up_data)
+    pdb_code = lowercase(match(r"([a-zA-Z0-9]{4})\.pdb", pdb_file).captures[1])
+    chain_match = match(r".*\.pdb_([a-zA-Z0-9])", pdb_file)
+    if chain_match !== nothing
+        chain_code = chain_match.captures[1]
+        ups = data[(data.PDB .== pdb_code) .& (data.CHAIN .== chain_code), :SP_PRIMARY]
+    else
+        ups = data[data.PDB .== pdb_code, :SP_PRIMARY]
+    end
+    unique(ups)
+end
+
+# create a function to look up the structures associated with a given UniProt accession
+function get_pdb_codes(uni_acc, data::DataFrames.DataFrame=sifts_up_data)
+    pdb_codes = data[data.SP_PRIMARY .== uni_acc, [:PDB, :CHAIN]]
+    uppercase.(unique(pdb_codes))
+end
 
 
 # delete the query if it is in the target column
-
-function delete_query_from_target!(search_results)
-    pdb = "$(pdb_code).pdb"
-    pdb_chain = "$(pdb_code).pdb_$(chain_code)"
-    filter!(row -> row.target != pdb && row.target != pdb_chain, search_results)
+function delete_query_from_target!(search_results, pdb_code, chain_code)
+    up = first(get_uniprot_acc("$(pdb_code).pdb_$(chain_code)"))
+    query_structures = get_pdb_codes(up)
+    for row in eachrow(query_structures)
+        pdb = "$(row[:PDB]).pdb"
+        pdb_chain = "$(row[:PDB]).pdb_$(row[:CHAIN])"
+        filter!(row -> row.target != pdb && row.target != pdb_chain, search_results)
+    end
+    search_results
 end
 
-delete_query_from_target!(search_results)
+delete_query_from_target!(search_results, pdb_code, chain_code)
 
-# delete all the rows with a sequence identity higher than 98%
-filter!(row -> row.fident < 0.98, search_results)
+# look for known conformations of the proteins showing similar structures to the query protein
+function list_known_conformations(search_results)
+    uniprots = Set{String}()
+    new_targets = Set{String}()
+    for row in eachrow(search_results)
+        pdb_code = match(r"([a-zA-Z0-9]{4})\.pdb", row.target).captures[1]
+        match_chain = match(r".*\.pdb_([a-zA-Z0-9])", row.target)
+        if match_chain !== nothing
+            chain_code = match_chain.captures[1]
+            target = "$(pdb_code).pdb_$(chain_code)"
+        else
+            target = "$(pdb_code).pdb"
+        end
+        ups = get_uniprot_acc(target)
+        for up in ups
+            push!(uniprots, up)
+        end
+    end
+    for up in uniprots
+        pdbs = get_pdb_codes(up)
+        for row in eachrow(pdbs)
+            pdb = row[:PDB]
+            chain = row[:CHAIN]
+            target = "$(pdb).pdb"
+            target_with_chain = "$(pdb).pdb_$(chain)"
+            if !(target in search_results.target) && !(target_with_chain in search_results.target)
+                push!(new_targets, target_with_chain)
+            end
+        end
+    end
+    new_targets
+end
 
-# TODO: Decide whether to use UniProt accessions to remove known conformations in the 
-# final pipeline. Consider making it optional.
+new_targets = list_known_conformations(search_results)
+
+targets = union(Set{String}(search_results.target), new_targets)
+
+# Create a folder with all the known targets (possible conformations of the query protein)
+
+# TODO: DRY
+function create_pdb_folder(targets::Set{String})
+    # PATH to the PDB files
+    pdb_folder = "/alpha/database/pdb/pdb_files"
+
+    # Create a local folder for PDB files 
+    # NOTE : It changes from first_targets to targets here
+    if isdir("targets")
+        rm("targets"; recursive=true)
+    end
+    mkdir("targets")
+    local_pdb_folder = abspath("targets")
+    
+    for target in targets
+        # Split target into pdb_code and chain_code
+        fields = split(target, '_')
+        pdb_code = String(fields[1])
+        if length(fields) == 2
+            chain_code = String(fields[2])
+        else
+            chain_code = MIToS.PDB.All
+        end
+        # Define original and local file paths
+        original_pdb_path = joinpath(pdb_folder, pdb_code)
+        local_pdb_path = joinpath(local_pdb_folder, target)
+        # Read only specific chain and write to local file
+        # occupancyfilter is needed to avoid the duplicated residue warnings with TMalign
+        chain = MIToS.PDB.read(original_pdb_path, MIToS.PDB.PDBFile, 
+            onlyheavy=true, occupancyfilter=true,
+            chain=chain_code)
+        MIToS.PDB.write(local_pdb_path, chain, MIToS.PDB.PDBFile)
+    end
+
+    return local_pdb_folder
+end
+
+local_pdb_folder = create_pdb_folder(targets)
+
+# use TMalign to cluster those structures
+
+function run_tmalign_and_cluster(local_pdb_folder::String, filename_prefix::String="tmalign")
+    # create a temporary file for the chain list
+    chain_list_path = tempname()
+    open(chain_list_path, "w") do io
+        for file in readdir(local_pdb_folder)
+            if occursin(".pdb", file) # only select PDB files
+                println(io, file)
+            end
+        end
+    end
+
+    # run TMalign with the -dir option:
+    dir_path = abspath(local_pdb_folder)
+    # add / at the end of the path if it is not there
+    if dir_path[end] != '/'
+        dir_path *= '/'
+    end
+    
+    stdout_path = filename_prefix * ".out"
+    stderr_path = filename_prefix * ".err"
+    
+    run(pipeline(`/store/EQUIPES/AMIG/MEMBERS/diego.zea/bin/TMalign -dir $dir_path $chain_list_path -fast`, 
+        stdout=stdout_path, stderr=stderr_path))
+    tmalign_out = read(stdout_path, String)
+    
+    tmalign_data = DataFrames.DataFrame(parse_tm_align_output(tmalign_out))
+
+    rmsd_mat = PairwiseListMatrices.from_table(tmalign_data[:,1:3], false)
+
+    rmsd_clust = Clustering.hclust(rmsd_mat, linkage=:complete, branchorder=:optimal)
+
+    rmsd_clusters = Clustering.cutree(rmsd_clust, h=1.0)
+
+    rmsd_chains = names(rmsd_mat)[1]
+    
+    # Return named tuple
+    return (tmalign_data = tmalign_data, rmsd_mat = rmsd_mat, rmsd_chains = rmsd_chains, rmsd_clusters = rmsd_clusters)
+end
+
+tmalign_results = run_tmalign_and_cluster("targets")
+
+# ---
+
+query_res = read(pdb_file, MIToS.PDB.PDBFile, group="ATOM", onlyheavy=true, occupancyfilter=true)
+query_seq = join(MIToS.MSA.three2residue(residue.id.name) for residue in query_res)
+
+if isdir("clusters")
+    rm("clusters"; recursive=true)
+end
+mkdir("clusters")
+
+for cluster in unique(tmalign_results.rmsd_clusters)
+    targets = tmalign_results.rmsd_chains[tmalign_results.rmsd_clusters .== cluster]
+    cluster_folder = "clusters/cluster_$(cluster)"
+    mkdir(cluster_folder)
+    open(joinpath(cluster_folder, "sequences.fasta"), "w") do file 
+        println(file, ">query")
+        println(file, query_seq)
+        for target in targets
+            res = read(joinpath(local_pdb_folder, target), MIToS.PDB.PDBFile, group="ATOM", onlyheavy=true, occupancyfilter=true)
+            println(file, ">$target")
+            for residue in res
+                print(file, MIToS.MSA.three2residue(residue.id.name))
+            end
+            print(file, "\n")
+        end
+    end
+    run(`clustalo -i $(joinpath(cluster_folder, "sequences.fasta")) -o $(joinpath(cluster_folder, "sequences.a3m")) --force --output-order=input-order`)
+    mkdir(joinpath(cluster_folder, "templates"))
+    pdbs = unique(String["$(split(pdb, '.')[1]).pdb" for pdb in targets])
+    for pdb in pdbs
+        cp(joinpath("/alpha/database/pdb/pdb_files", pdb), joinpath(cluster_folder, "templates", pdb))
+    end
+end
+
