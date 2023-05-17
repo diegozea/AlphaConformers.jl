b2e90247788b1f45194f1ab7e807310b987f4a1e
Diego Javier Zea <diegozea@gmail.com>
2023-05-17 16:23:58 +0200

    some runs

---
+++ scripts/run_alphafold.jl
@@ -0,0 +1,23 @@
+# diego.zea@node48:/alpha/runs/diego.zea$ cp -r from_apo /store/EQUIPES/AMIG/MEMBERS/diego.zea/AlphaConformers
+
+const WORKING_DIR = "/store/EQUIPES/AMIG/MEMBERS/diego.zea/AlphaConformers/from_apo"
+
+const folders = ["1AEL_A", "1BV2_A", "1C54_A", "1EAL_A"]
+
+function run_alphafold(folder)
+    cd(WORKING_DIR)
+    cd(folder)
+    cd("clusters")
+    subfolders = filter(f -> isdir(f) && startswith(f, "cluster_"), 
+        readdir())
+    for subfolder in subfolders
+        cd(subfolder)
+        run(`/opt/alphafold/runcolabfold.py sequences.a3m af --use-templates 1 --msa-input --custom-template-path templates/ --num-seeds 5 --use-dropout --num-models 2`)
+        cd("..")
+    end
+    cd("..")
+end
+
+for folder in folders
+    run_alphafold(folder)
+end
