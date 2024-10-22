9d34e5354e02d70e9fc23826cc4bd3777fb64053
Diego Javier Zea <diegozea@gmail.com>
2024-10-22 15:39:10 +0200

    + MIToS.MSA. to sequencenames

--- src/foldseek.jl
+++ src/foldseek.jl
@@ -587,7 +587,7 @@ function get_cluster2msa(msa, cluster2seqnames)
         catch err
             @error "Error ($err) getting the MSA for cluster $cluster"
             @info "seqnames: $seqnames"
-            @info "msa names: $(sequencenames(msa))"
+            @info "msa names: $(MIToS.MSA.sequencenames(msa))"
             rethrow(err)
         end
     end
