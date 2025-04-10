bd6539c2555ea891e74bfcdd3f5aaf95c8ed6e0b
ZulieD <julie.daniel@efrei.net>
2025-04-10 10:56:47 +0200

    Change rsync to download with wget

--- scripts/download_sift.jl
+++ scripts/download_sift.jl
@@ -14,6 +14,7 @@ using MIToS.PDB
 using MIToS.SIFTS
 using DataFrames
 import CSV
+@info "START"
 sift_directory = abspath("/alpha/database", "sift")
 isdir(sift_directory) || mkdir(sift_directory)
 output_directory = abspath(sift_directory, "sift_files")
@@ -26,57 +27,11 @@ file_path_df_final="pdb_information_details.csv"
 df_final=DataFrames.DataFrame(CSV.File(file_path_df_final,
 comment="#", missingstring=["", "None"])) # Output DF with PDB CHAIN RESOLUTION SITE LIGAND
 
-@info "START"
-#For each pdb 
-sift_not_download=[]
-pdb_not_download=[]
-for row in eachrow(df_final)
-    pdb=row.PDB
-    #Check if xml file exist 
-    siftsfile=joinpath(output_directory,pdb*".xml.gz")
-
-    if !isfile(siftsfile) #if it is not already downloaded
-        @info "The file doesn't exist. Downloading the file XML..."
-
-        # Prépare la commande rsync
-        remote_path = "rsync://ftp.ebi.ac.uk/pub/databases/msd/sifts/xml/$pdb.xml.gz"
-        local_path = output_directory
-
-        try
-            run(`rsync -av $remote_path $local_path`)
-            if isfile(siftsfile)
-                @info "✅ Successfully downloaded $pdb.xml.gz using rsync"
-            else
-                push!(sift_not_download, pdb)
-                @warn "❌ File not found after rsync: $pdb.xml.gz"
-                push!(sift_not_download,pdb)
-            end
-        catch e
-            push!(sift_not_download, pdb)
-            @warn "❌ Error during rsync for $pdb: $e"
-            push!(sift_not_download,pdb)
-        end
-    end
-
-    #Check if the pdb is download
-    pdbfile=joinpath(output_directory_pdb,pdb*".pdb")
-    if !isfile(pdbfile) #if it is not already downloaded
-        push!(pdb_not_download,pdb)
-    end
+try
+    run(`wget -r -np -nH --cut-dirs=5 -P $output_directory ftp://ftp.ebi.ac.uk/pub/databases/msd/sifts/xml/`)
+    @info "✅ Tous les fichiers XML SIFTS ont été téléchargés dans $output_directory"
+catch e
+    @warn "❌ Erreur pendant le téléchargement via wget: $e"
 end
 
-#Create a file with the pdb error 
-open("Error_downloading_sift_pdb.txt", "w") do io
-    println(io, "SIFT file that couldn't be download by download_sift.jl :")
-    for item in sift_not_download
-        println(io, item)
-    end
-
-    println(io, "")  
-
-    println(io, "PDB file that aren't download in pdb_files :")
-    for item in pdb_not_download
-        println(io, item)
-    end
-end
 @info "END !"
